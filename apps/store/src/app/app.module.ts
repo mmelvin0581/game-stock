0f2a1fe0670480ae3e5dc52e37ee103466bf0499
Michael Melvin <michael.melvin@pwc.com>
2021-02-22 11:29:19 -0500

    03-GenerateAComponentLibrary

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from '@game-stock/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     MatCardModule,
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
